2c184e598a347c1c03ecedf09b99c5a632e8efc1
yosrio <yosrio71@gmail.com>
2026-06-01 09:47:23 +0700

    config: base path / for yosrio.github.io user page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/v2/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
